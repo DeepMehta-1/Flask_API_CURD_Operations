d90da9c1b246fe08123b9cb56e020cb5c934b2ae
DeepMehta-1 <92302299+DeepMehta-1@users.noreply.github.com>
2022-04-09 22:27:12 -0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,21 @@
+from settings import db
+from sqlalchemy import Column, Integer, Text
+from datetime import datetime
+
+
+class User(db.Model):
+
+    student_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(60))
+    last_name = db.Column(db.String(60))
+    dob = db.Column(db.String(60))
+    amount_due = db.Column(db.String(300))
+    # create_at = db.Column(
+    #     db.TIMESTAMP,
+    #     default=datetime.utcnow,
+    #     nullable=False
+    #   )
+
+    def __str__(self):
+        return self.first_name + self.last_name + str(self.dob)
+
---
+++ settings.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# define sqlite database file path 
+db_dir = os.path.abspath('data.sqlite')
+
+# app settings 
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+# Database connecting for Windows
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+db_dir
+db = SQLAlchemy(app)
